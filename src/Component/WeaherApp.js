f3202539f3625e491a26bab00e763914519ee7ec
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-22 22:04:18 +0900

    component Creation Finish

--- src/Component/WeaherApp.js
+++ src/Component/WeaherApp.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { App_Id } from "./lib/API_ID/app_id";
-
+import "./Wthr.css";
 export default class Weather extends React.Component {
-  state = {
-    locate: "",
-    wthr: "",
-    discript: "",
-    temp: "",
-    wind: "",
-    icon: null
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      locate: "",
+      wthr: "",
+      discript: "",
+      temp: "",
+      wind: "",
+      icon: null
+    };
+  }
 
+  // HTML5 내장 API 사용
   componentDidMount() {
     const option = {
       enableHighAccuracy: true,
@@ -20,7 +24,7 @@ export default class Weather extends React.Component {
     navigator.geolocation.getCurrentPosition(
       position => {
         console.log(position);
-        this.getWthr(position.coords, position.coords);
+        this.getWthr(position.coords, position.coords); //내장 API정보를 getWthr 함수로 전달
       },
       error => {
         console.log("위치확인 불가", error);
@@ -29,11 +33,12 @@ export default class Weather extends React.Component {
     );
   }
 
+  // 위도, 경도를 내장 API로 부터 받아서 정보 요청
   getWthr = ({ latitude, longitude }) => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${App_Id}&units=metric`
     )
-      .then(res => res.json())
+      .then(res => res.json()) //json문서로 변환
       .then(json => {
         //console.log(json);
         this.setState({
@@ -43,14 +48,14 @@ export default class Weather extends React.Component {
           temp: json.main.temp,
           wind: json.wind.speed,
           icon: json.weather[0].icon
-        });
+        }); // json문서의 정보를 상태에 Set
 
         console.log(this.state);
       });
   };
 
   render() {
-    const { locate, wthr, temp, wind, icon } = this.state;
+    const { locate, wthr, temp, wind, icon, descript } = this.state;
     return (
       <div>
         <div className="container">
@@ -58,25 +63,22 @@ export default class Weather extends React.Component {
             <div className="title">
               <h1>Current Weather</h1>
             </div>
-            <ul>
-              <li>
-                <p>{locate}</p>
-              </li>
-              <li>
-                <p>{wthr}</p>
-              </li>
-              <li>
-                <p>{temp}</p>
-              </li>
-              <li>
-                <p>{wind}</p>
-              </li>
-            </ul>
-            <div className="whtr-icon">
-              <img
-                src={`http://openweathermap.org/img/w/${icon}.png`}
-                alt="wthr_img"
-              />
+            <div className="wthr-info">
+              <div className="info-container">
+                <div>위치: {locate}</div>
+                <div>
+                  날씨: {wthr}({descript})
+                </div>
+                <div>기온: {temp}</div>
+                <div>풍속: {wind}</div>
+              </div>
+              <div className="whtr-icon">
+                <img
+                  src={`http://openweathermap.org/img/w/${icon}.png`}
+                  alt="wthr_img"
+                  style={{ width: "200px", height: "200px" }}
+                />
+              </div>
             </div>
           </section>
         </div>
